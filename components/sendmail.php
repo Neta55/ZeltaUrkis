014e9173c787c90ce67310370357baa5d3f1f6ff
Ineta Priga <ineta.priga@gmail.com>
2021-01-28 22:19:52 +0200

    rec changed

--- components/sendmail.php
+++ components/sendmail.php
@@ -1,20 +1,5 @@
 <?php
-// $recaptcha = $_POST['g-recaptcha-response'];
 
-// if(!empty($recaptcha)) {
-//     $recaptcha = $_REQUEST['g-recaptcha-response'];
-//     $secret = 'secret key';
-//     $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secret ."&response=".$recaptcha."&remoteip=".$_SERVER['REMOTE_ADDR'];
-//     $curl = curl_init();
-//     curl_setopt($curl, CURLOPT_URL, $url);
-//     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-//     curl_setopt($curl, CURLOPT_TIMEOUT, 10);
-//     curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0");
-//     $curlData = curl_exec($curl);
-//     curl_close($curl); 
-//     $curlData = json_decode($curlData, true);
-//     if($curlData['success']) {
-// pieliekot robotu apakšā 1 rinda dzēšās
 
 if (isset($_POST['name'])) {
 
