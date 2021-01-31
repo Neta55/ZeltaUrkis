95686704fd1ec4305888b7ea0539e5872e5fd4f4
Ineta Priga <ineta.priga@gmail.com>
2021-01-31 04:49:42 +0200

    galerija pievienota

--- js/main.js
+++ js/main.js
@@ -25,3 +25,8 @@ function showHeroSlides(n) {
 
 
 // })
+
+// Set a same-site cookie for first-party contexts
+document.cookie = 'cookie1=value1; SameSite=Lax';
+// Set a cross-site cookie for third-party contexts
+document.cookie = 'cookie2=value2; SameSite=None; Secure';
