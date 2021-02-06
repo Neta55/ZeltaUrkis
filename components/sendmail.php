13acfb2f6be82f18feeba86629b34328f8294ca2
Ineta Priga <ineta.priga@gmail.com>
2021-02-06 20:30:07 +0200

    novākta abs saikne

--- components/sendmail.php
+++ components/sendmail.php
@@ -20,12 +20,12 @@ if (isset($_POST['name'])) {
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="/ZeltaUrkis/css/style.css">
+      <link rel="stylesheet" href="../css/style.css">
       <title>Pasūtīt Zelta Urķi</title>
-      <link rel="apple-touch-icon" sizes="180x180" href="/ZeltaUrkis/css/img/logo/favicon/apple-touch-icon.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="/ZeltaUrkis/css/img/logo/favicon/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="/ZeltaUrkis/css/img/logo/favicon/favicon-16x16.png">
-      <link rel="manifest" href="/ZeltaUrkis/css/img/logo/favicon/site.webmanifest">
+      <link rel="apple-touch-icon" sizes="180x180" href="../css/img/logo/favicon/apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="../css/img/logo/favicon/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="../css/img/logo/favicon/favicon-16x16.png">
+      <link rel="manifest" href="../css/img/logo/favicon/site.webmanifest">
       <link rel="preconnect" href="https://fonts.gstatic.com">
       <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet">
     </head>
@@ -34,7 +34,7 @@ if (isset($_POST['name'])) {
     echo "<div id='cover'>";
     echo "<div id='pop-up'>";
     echo "<h3><strong>" . $name . "</strong>, tavs pieprasījums ir veiksmīgi aizsūtīts.</br>Mēs ar tevi sazināsimies tuvākajā laikā!</h3>";
-    echo "<div class='button'><a href='/ZeltaUrkis/index.html'>OK</a></div>";
+    echo "<div class='button'><a href='../index.html'>OK</a></div>";
     echo "</div>";
     echo "</div>";
     echo "</body>";
@@ -47,12 +47,12 @@ if (isset($_POST['name'])) {
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="/ZeltaUrkis/css/style.css">
+      <link rel="stylesheet" href="../css/style.css">
       <title>Pasūtīt Zelta Urķi</title>
-      <link rel="apple-touch-icon" sizes="180x180" href="/ZeltaUrkis/css/img/logo/favicon/apple-touch-icon.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="/ZeltaUrkis/css/img/logo/favicon/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="/ZeltaUrkis/css/img/logo/favicon/favicon-16x16.png">
-      <link rel="manifest" href="/ZeltaUrkis/css/img/logo/favicon/site.webmanifest">
+      <link rel="apple-touch-icon" sizes="180x180" href="../css/img/logo/favicon/apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="../css/img/logo/favicon/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="../css/img/logo/favicon/favicon-16x16.png">
+      <link rel="manifest" href="../css/img/logo/favicon/site.webmanifest">
       <link rel="preconnect" href="https://fonts.gstatic.com">
       <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet">
     </head>
@@ -65,9 +65,9 @@ if (isset($_POST['name'])) {
     echo "<div id='pop-up'>";
     echo "<h3>Ups!</br>Kaut kas nav tā ar tiem sakariem.</br>Pasūti Urķi, uzrakstot mums</br>e-pastu <strong>zeltaurkis@gmail.com</strong>!</h3>";
 
-    echo "<div class='button'><a href='/ZeltaUrkis/index.html'>ATGRIEZTIES SĀKUMLAPĀ</a></div>";
+    echo "<div class='button'><a href='../index.html'>ATGRIEZTIES SĀKUMLAPĀ</a></div>";
     echo "</br>";
-    echo "<div class='button'><a href='/ZeltaUrkis/components/order-form-lv.html'>MĒĢINĀT VĒLREIZ</a></div>";
+    echo "<div class='button'><a href='../components/order-form-lv.html'>MĒĢINĀT VĒLREIZ</a></div>";
     echo "</div>";
     echo "</div>";
     echo "</body>";
